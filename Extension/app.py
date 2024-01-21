c5e2c4e7ff2dffaece38867e21712536cb723ad0
MuzzammilRafiq <mzmlrafiqcse@gmail.com>
2024-01-21 19:00:42 +0530

    init

---
+++ Extension/app.py
@@ -0,0 +1,187 @@
+from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+import numpy as np
+
+# Keras
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.models import load_model
+from keras.preprocessing import image
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+import sqlite3
+import pandas as pd
+import numpy as np
+import pickle
+import sqlite3
+import random
+
+import smtplib 
+from email.message import EmailMessage
+from datetime import datetime
+
+app = Flask(__name__)
+
+from keras import backend as K
+
+def recall_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+def precision_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+def f1_m(y_true, y_pred):
+    precision = precision_m(y_true, y_pred)
+    recall = recall_m(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
+
+UPLOAD_FOLDER = 'static/uploads/'
+
+# allow files of a specific type
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+# function to check the file extension
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+model_path2 = 'mobile.h5' # load .h5 Model
+
+CTS = load_model(model_path2, compile=False)
+from keras.preprocessing.image import load_img, img_to_array
+
+def model_predict2(image_path,model):
+    print("Predicted")
+    image = load_img(image_path,target_size=(128,128))
+    image = img_to_array(image)
+    image = image/255
+    image = np.expand_dims(image,axis=0)
+    
+    result = np.argmax(model.predict(image))
+     
+    
+    if result == 0:
+        return "GLIOMA TUMOR! THE PATIENT HAVE BEEN SUFFER FROM BRAIN DISEASE","after.html"        
+    elif result == 1:
+        return "PITUITARY TUMOR! THE PATIENT DON'T SUFFER FROM BRAIN DISEASE","after.html"
+   
+    
+@app.route('/predict2',methods=['GET','POST'])
+def predict2():
+    print("Entered")
+    
+    print("Entered here")
+    file = request.files['files'] # fet input
+    filename = file.filename        
+    print("@@ Input posted = ", filename)
+        
+    file_path = os.path.join(UPLOAD_FOLDER, filename)
+    file.save(file_path)
+
+    print("@@ Predicting class......")
+    pred, output_page = model_predict2(file_path,CTS)
+              
+    return render_template(output_page, pred_output = pred, img_src=UPLOAD_FOLDER + file.filename)
+
+@app.route("/notebook")
+def notebook():
+    return render_template("notebook.html")
+
+@app.route("/graph")
+@app.route("/about")
+def graph():
+    return render_template("graph.html")
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+	return render_template('home.html')
+
+
+@app.route('/logon')
+def logon():
+	return render_template('signup.html')
+
+@app.route('/login')
+def login():
+	return render_template('signin.html')
+
+
+
+@app.route('/index')
+def index():
+	return render_template('index.html')
+
+@app.route("/signup")
+def signup():
+    global otp, username, name, email, number, password
+    username = request.args.get('user','')
+    name = request.args.get('name','')
+    email = request.args.get('email','')
+    number = request.args.get('mobile','')
+    password = request.args.get('password','')
+    otp = random.randint(1000,5000)
+    print(otp)
+    msg = EmailMessage()
+    msg.set_content("Your OTP is : "+str(otp))
+    msg['Subject'] = 'OTP'
+    msg['From'] = "evotingotp4@gmail.com"
+    msg['To'] = email
+    
+    
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login("evotingotp4@gmail.com", "xowpojqyiygprhgr")
+    s.send_message(msg)
+    s.quit()
+    return render_template("val.html")
+
+@app.route('/predict1', methods=['POST'])
+def predict1():
+    global otp, username, name, email, number, password
+    if request.method == 'POST':
+        message = request.form['message']
+        print(message)
+        if int(message) == otp:
+            print("TRUE")
+            con = sqlite3.connect('signup.db')
+            cur = con.cursor()
+            cur.execute("insert into `info` (`user`,`email`, `password`,`mobile`,`name`) VALUES (?, ?, ?, ?, ?)",(username,email,password,number,name))
+            con.commit()
+            con.close()
+            return render_template("signin.html")
+    return render_template("signup.html")
+
+@app.route("/signin")
+def signin():
+
+    mail1 = request.args.get('user','')
+    password1 = request.args.get('password','')
+    con = sqlite3.connect('signup.db')
+    cur = con.cursor()
+    cur.execute("select `user`, `password` from info where `user` = ? AND `password` = ?",(mail1,password1,))
+    data = cur.fetchone()
+
+    if data == None:
+        return render_template("signin.html")    
+
+    elif mail1 == str(data[0]) and password1 == str(data[1]):
+        return render_template("index.html")
+    else:
+        return render_template("signin.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
